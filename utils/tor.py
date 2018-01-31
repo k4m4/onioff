98d89dafb869a3cceb63466a26c2c5769737cd9a
Shahin <me@5hah.in>
2018-01-31 17:14:53 +0330

    Directly bootstrap a tor process to handle requests
    This way we don't need to check IP addresses multiple time which speeds up the process

--- utils/tor.py
+++ utils/tor.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 
 import requests
-from requests.exceptions import ConnectionError, HTTPError
+import stem.process
 
 from utils.html import get_html_element
 
@@ -9,60 +9,52 @@ onion_status = namedtuple('onion_status', ['active', 'title'])
 
 
 class Tor:
-    def __init__(self, ipcheck_url=None, tor_host='127.0.0.1',
-                 tor_port='9050'):
+    def __init__(self, socks_port='7000', config=None):
+        self._tor_process = None
+        self._proxies = None
 
-        if ipcheck_url:
-            self.ipcheck_url = ipcheck_url
+        self.socks_port = socks_port
+
+        if not config or not isinstance(config, dict):
+            self.config = {
+                'SocksPort': str(self.socks_port),
+            }
         else:
-            self.ipcheck_url = 'https://locate.now.sh/ip'
+            config['SocksPort'] = socks_port
+            self.config = config
 
-        self.tor_host = tor_host
-        self.tor_port = tor_port
-        self.pure_ip = self.get_ip(False)
-        self.tor_outgoing_ip = None
+    def __enter__(self):
+        self.connect()
+        return self
 
-    def get_ip(self, proxied=True):
-        """ Get outgoing user's IP address using ipcheck_url service
-        """
-        proxies = {}
-        if proxied:
-            proxies = self.get_proxies()
-
-        response = requests.get(self.ipcheck_url, proxies=proxies)
-
-        status = response.status_code
-        if status == 200:
-            return response.text.replace('\n', '')
-        else:
-            msg = "Can't get the IP, status code = {}".format(status)
-            raise HTTPError(msg)
-
-    def get_proxies(self, remote_dns=True):
-        protocol = 'socks5h'
-        if not remote_dns:
-            protocol = 'socks5'
-
-        tor_proxy = '{}://{}:{}'.format(protocol, self.tor_host, self.tor_port)
-        return {'http': tor_proxy, 'https': tor_proxy}
+    def __exit__(self, *args):
+        self.disconnect()
 
     def connect(self):
-        """Check if user is connected to Tor network by comparing current IP address to
-        the value of self.pure_ip
+        def print_bootstrap_lines(line):
+            if "Bootstrapped " in line:
+                print(line)
 
-        """
-        self.tor_outgoing_ip = self.get_ip()
-        if self.pure_ip == self.tor_outgoing_ip:
-            raise ConnectionError('Unsuccessful Tor connection')
+        self._tor_process = stem.process.launch_tor_with_config(
+            config=self.config,
+            init_msg_handler=print_bootstrap_lines,
+        )
+
+        tor_proxy = 'socks5h://localhost:{}'.format(self.socks_port)
+        self._proxies = {'http': tor_proxy, 'https': tor_proxy}
+
+    def disconnect(self):
+        self._tor_process.kill()
+        self._proxies = None
 
     def get(self, url, params=None):
-        if not self.tor_outgoing_ip:
+        if not self._tor_process:
             self.connect()
 
         if not params or not isinstance(params, dict):
             params = {}
 
-        return requests.get(url, params=params, proxies=self.get_proxies())
+        return requests.get(url, params=params, proxies=self._proxies)
 
     def check_onion(self, onion):
         response = self.get(onion)
