690a179e865da3e9b5201b80caa7f6af6b45c7ea
Shahin <me@5hah.in>
2018-02-01 17:23:18 +0330

    Write outputs in two different supported formats (JSON, TSV)

--- onioff.py
+++ onioff.py
@@ -6,6 +6,8 @@ Copyright (C) 2016-2018 Nikolaos Kamarinakis (nikolaskam@gmail.com)
 See License at nikolaskama.me (https://nikolaskama.me/onioffproject)
 """
 
+import csv
+import json
 import os
 
 import click
@@ -57,6 +59,11 @@ def read_file(path):
 @click.option('--input-file', '-f', help='input file name')
 @click.option('--output', '-o', help='output file name')
 @click.option(
+    '--format',
+    type=click.Choice(['json', 'tsv']),
+    help='output format',
+    default='tsv')
+@click.option(
     '--socks-port',
     '-p',
     help="Tor instance's port number (default={})".format(SOCKS_PORT_DEFAULT),
@@ -73,11 +80,14 @@ def read_file(path):
     help='number of concurrent workers (default={})'.format(WORKERS_DEFAULT),
     default=WORKERS_DEFAULT)
 @click.option(
+    '--quite', '-q', help='do not log on stdout', is_flag=True, default=False)
+@click.option(
     '--no-banner',
     help='do not display the banner',
     is_flag=True,
     default=False)
-def main(url, input_file, output, socks_port, timeout, workers, no_banner):
+def main(url, input_file, output, format, socks_port, timeout, workers, quite,
+         no_banner):
 
     if not no_banner:
         print_banner()
@@ -103,14 +113,44 @@ def main(url, input_file, output, socks_port, timeout, workers, no_banner):
             url_list, timeout=timeout, workers=workers)
 
     logger.debug("[!] Onion inspection successfully complete")
-    for item in results:
-        if not item.status:
-            logger.error('Failed [{}]: {}'.format(item.url, item.title))
-        elif item.active:
-            logger.info('Active [{}]: {}'.format(item.url, item.title))
-        else:
-            logger.warn('Inactive [{}]: Status Code {}'.format(
-                item.url, item.status))
+    if not quite:
+        for item in results:
+            if not item.status:
+                logger.error('Failed [{}]: {}'.format(item.url, item.title))
+            elif item.active:
+                logger.info('Active [{}]: {}'.format(item.url, item.title))
+            else:
+                logger.warn('Inactive [{}]: Status Code {}'.format(
+                    item.url, item.status))
+
+    if output:
+        if os.path.exists(output):
+            prompt = '{} already exists, can I overwrite?'.format(output)
+            if not click.confirm(prompt):
+                logger.warn('No output file generated!')
+                os._exit(1)
+
+        if format == 'tsv':
+            logger.debug('Writing result to {} as TSV.'.format(output))
+            header = ['Onion', 'Status Code', 'Active', 'Title/Message']
+            with open(output, 'wb') as csvfile:
+                writer = csv.writer(csvfile, delimiter='\t')
+                writer.writerow(header)
+                for item in results:
+                    writer.writerow(
+                        [item.url, item.status, item.active, item.title])
+        if format == 'json':
+            logger.debug('Writing result to {} as JSON.'.format(output))
+            obj = []
+            for item in results:
+                obj.append({
+                    'onion': item.url,
+                    'status_code': item.status,
+                    'active': item.active,
+                    'title': item.title
+                })
+            with open(output, 'wb') as fp:
+                json.dump(obj, fp)
 
 
 if __name__ == '__main__':
